0c1bea9e3214c9c3f552fcd76375d0395a3650bc
vishwanthieherath <vishwanthieherath@gmail.com>
2024-12-01 02:08:01 +0530

    Add model routes

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const morgan = require('morgan');
 
 const adminRoutes = require('./src/routes/adminRoute');
+const modelRoutes = require('./src/routes/r2Route');
 
 require('dotenv').config();
 
@@ -15,6 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
 app.use('/api/users', adminRoutes);
+app.use("/api/models", modelRoutes);
 
 app.listen(PORT, () => {
     console.log(`API Gateway server is running on http://localhost:${PORT}`);
